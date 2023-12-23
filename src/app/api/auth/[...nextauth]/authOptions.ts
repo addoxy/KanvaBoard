79b20c0881cc120944f83efb3bffa8c11a2cedf0
addoxy <adityakumar13043@gmail.com>
2023-12-23 16:47:21 +0530

    Add next-auth api route

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/app/utils/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const adapter = PrismaAdapter(prisma) as Adapter;
+
+export const authOptions: NextAuthOptions = {
+  adapter: adapter,
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID || "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in",
+  },
+};
