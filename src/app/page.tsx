84eadc462efad33760ad3c5809ed5398b2c07544
addoxy <adityakumar13043@gmail.com>
2023-12-23 18:38:40 +0530

    Remove debugging data from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/authOptions";
-
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  return (
-    <div className="flex flex-col text-xl text-white">
-      <pre>{JSON.stringify(session)}</pre>
-      {!session && <p>Not logged in!</p>}
-    </div>
-  );
+  return <></>;
 }
