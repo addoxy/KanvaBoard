5275bf4ec16fd67219ff67fc486feb94017c911c
addoxy <adityakumar13043@gmail.com>
2023-12-23 23:32:25 +0530

    Create templates page

---
+++ src/app/templates/page.tsx
@@ -0,0 +1,9 @@
+import Title from "@/components/Title";
+
+export default function page() {
+  return (
+    <div className="flex flex-col">
+      <Title text="Templates" variant="xl" />
+    </div>
+  );
+}
