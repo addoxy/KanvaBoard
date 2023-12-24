697eec62c3282b7f395eac021ba7d9f0174a9093
addoxy <adityakumar13043@gmail.com>
2023-12-24 20:32:56 +0530

    Add a dropdown menu for project card component

--- src/app/projects/components/OptionsMenu.tsx
+++ src/app/projects/components/OptionsMenu.tsx
@@ -9,7 +9,34 @@ const OptionsMenu = () => {
           <ThreeDotsIcon className="w-6 h-6 text-zinc-300" />
         </button>
       </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          className="w-40 bg-zinc-800 border border-zinc-700 rounded-md flex flex-col"
+          align="end"
+        >
+          <DropdownMenu.Item>
+            <OptionItem text="Add to favorites" />
+          </DropdownMenu.Item>
+          <DropdownMenu.Item>
+            <OptionItem text="Delete" />
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
     </DropdownMenu.Root>
+  );
+};
+
+interface OptionItem {
+  text: string;
+}
+
+const OptionItem = (props: OptionItem) => {
+  const { text } = props;
+
+  return (
+    <button className="h-11 px-4 w-full text-left text-zinc-400 hover:bg-zinc-700 hover:text-zinc-300">
+      {text}
+    </button>
   );
 };
 
