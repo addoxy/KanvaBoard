5515014cfabde815e1b2df7ae8a73a3cc6168c41
addoxy <adityakumar13043@gmail.com>
2023-12-24 15:00:31 +0530

    Change task card's background and border

--- src/app/projects/board/[id]/components/Task.tsx
+++ src/app/projects/board/[id]/components/Task.tsx
@@ -1,6 +1,6 @@
 const Task = () => {
   return (
-    <p className="cursor-pointer rounded-lg border border-zinc-700/50 bg-zinc-750 p-3 text-sm text-zinc-300">
+    <p className="cursor-pointer rounded-lg border border-zinc-700/20 bg-zinc-700/30 p-3 text-sm text-zinc-300">
       Task content
     </p>
   );
