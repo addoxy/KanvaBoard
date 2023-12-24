a65cbf7d9961f4963efa97de71a465dc4f43b199
addoxy <adityakumar13043@gmail.com>
2023-12-24 14:54:09 +0530

    Update routes and fix error due to drag icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -25,9 +25,9 @@ export const DragIcon = (props: IconProps) => {
     >
       <path
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M22 8a2 2 0 1 1-4 0a2 2 0 0 1 4 0m0 8a2 2 0 1 1-4 0a2 2 0 0 1 4 0m-8 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0M6 8a2 2 0 1 1-4 0a2 2 0 0 1 4 0m0 8a2 2 0 1 1-4 0a2 2 0 0 1 4 0m8-8a2 2 0 1 1-4 0a2 2 0 0 1 4 0"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
