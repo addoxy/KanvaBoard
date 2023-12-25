e4048caec433d5f77b0b23202e39d93279df55d3
addoxy <adityakumar13043@gmail.com>
2023-12-25 16:20:48 +0530

    Update the not-found page to go to the home page or projects page based on user session

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,19 @@
 import logo from "@/../public/logo.png";
 import Title from "@/components/Title";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
+import { authOptions } from "./api/auth/[...nextauth]/authOptions";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(authOptions);
+  const link = session ? "/projects" : "/";
+
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen">
       <Image src={logo} alt="logo" className="mb-20" />
       <Title text="Page not found" variant="xl" className="mb-6" />
-      <Link href="/">
+      <Link href={link}>
         <button className="bg-violet-700 text-zinc-300 text-sm shrink-0 w-44 h-11 rounded-lg font-medium">
           Return Home
         </button>
