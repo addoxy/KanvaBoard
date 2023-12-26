7337a6d08eb0998f6f3def323922602e76599064
addoxy <adityakumar13043@gmail.com>
2023-12-26 16:43:20 +0530

    Update profile menu to include user's name and email

--- src/components/Sidebar/ProfileMenu.tsx
+++ src/components/Sidebar/ProfileMenu.tsx
@@ -36,12 +36,24 @@ const ProfileMenu = () => {
         </DropdownMenu.Trigger>
       )}
       <DropdownMenu.Portal>
-        <DropdownMenu.Content align="center" sideOffset={8}>
+        <DropdownMenu.Content
+          align="center"
+          sideOffset={8}
+          className="w-44 shadow-md bg-zinc-800 border border-zinc-700/25 rounded-lg overflow-hidden pt-3"
+        >
+          <DropdownMenu.Item className="flex flex-col outline-none px-4 border-b border-b-zinc-700/50">
+            <span className="truncate text-zinc-300 mb-1 text-sm">
+              {session?.user?.name}
+            </span>
+            <span className="truncate text-zinc-400 mb-4 text-xs">
+              {session?.user?.email}
+            </span>
+          </DropdownMenu.Item>
           <DropdownMenu.Item className="outline-none">
             <button
               onClick={() => signOut({ callbackUrl: "/signout" })}
               className={
-                "text-zinc-500 font-medium flex items-center text-sm w-44 h-10 shadow-md bg-zinc-800 border border-zinc-700/25 rounded-lg pl-5 hover:bg-zinc-700 hover:text-zinc-400"
+                "text-zinc-500 w-full font-medium flex items-center text-sm h-10  hover:bg-zinc-700 hover:text-zinc-400 px-4"
               }
             >
               <SignoutIcon className="w-4 h-4 mr-3 shrink-0" />
