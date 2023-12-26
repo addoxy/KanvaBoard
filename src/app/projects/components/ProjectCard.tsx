51de9eea5658accbd10c4c595f0319a039c987d0
addoxy <adityakumar13043@gmail.com>
2023-12-26 17:11:35 +0530

    Update the styling of project card and project header

--- src/app/projects/components/ProjectCard.tsx
+++ src/app/projects/components/ProjectCard.tsx
@@ -14,11 +14,11 @@ const ProjectCard = (props: ProjectCardProps) => {
   const { id, name, lastViewed, href, favorite } = props;
   return (
     <TableRow className="text-zinc-400 hover:bg-zinc-800/20 border-b-zinc-700/30">
-      <TableCell className="pl-6 py-7">
+      <TableCell className="pl-6 py-7 text-sm 600 w-7/12">
         <Link href={href}>{name}</Link>
       </TableCell>
-      <TableCell className="py-7">{lastViewed}</TableCell>
-      <TableCell className="py-7 pr-6">
+      <TableCell className="py-7 text-sm 600 w-4/12">{lastViewed}</TableCell>
+      <TableCell className="py-7 w-1/12">
         <OptionsMenu favorite={favorite} id={id} />
       </TableCell>
     </TableRow>
--- src/app/projects/components/ProjectHeader.tsx
+++ src/app/projects/components/ProjectHeader.tsx
@@ -4,13 +4,13 @@ const ProjectHeader = () => {
   return (
     <TableHeader>
       <TableRow className="border-b-zinc-700/30">
-        <TableHead className="text-sm text-zinc-600 pl-6 pb-5 w-160">
+        <TableHead className="text-sm text-zinc-600 pl-6 w-7/12 pb-5">
           Title
         </TableHead>
-        <TableHead className="text-sm w-120 font-medium text-zinc-600 pb-5">
+        <TableHead className="text-sm w-4/12 font-medium text-zinc-600 pb-5">
           Last Viewed At
         </TableHead>
-        <TableHead className="mr-6 pb-5 pr-6"></TableHead>
+        <TableHead className="w-1/12"></TableHead>
       </TableRow>
     </TableHeader>
   );
