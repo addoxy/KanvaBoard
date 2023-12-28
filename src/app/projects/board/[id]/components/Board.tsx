8e5de955b32c3b07556edfa65fe58f70bc252612
addoxy <adityakumar13043@gmail.com>
2023-12-28 17:24:55 +0530

    Implement the spacer component on the board component

--- src/app/projects/board/[id]/components/Board.tsx
+++ src/app/projects/board/[id]/components/Board.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/Button";
+import Spacer from "@/components/Spacer";
 import Title from "@/components/Title";
 import { useState } from "react";
 import Column from "./Column";
@@ -29,7 +30,7 @@ const Board = (props: BoardProps) => {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex justify-between items-center mb-16">
+      <div className="flex justify-between items-center">
         <Title text={title} variant="xl" className="line-clamp-1" />
         <Button
           text="+ New Column"
@@ -37,6 +38,7 @@ const Board = (props: BoardProps) => {
           handleClick={handleSetColumns}
         />
       </div>
+      <Spacer variant="lg" />
       <div className="flex gap-x-6 -mr-12 -ml-12 scrollbar-thin scrollbar-thumb-zinc-800 scrollbar-track-zinc-850 hover:scrollbar-thumb-zinc-700 scrollbar-round overflow-x-auto h-full">
         {columns.map((column) => (
           <Column {...column} key={column.id} />
