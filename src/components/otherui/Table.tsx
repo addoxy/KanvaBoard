ce5bdf2b74e30b695f9b04572efb43caa37072b3
addoxy <adityakumar13043@gmail.com>
2023-12-29 14:22:28 +0530

    Add animation to project cards

--- src/components/otherui/Table.tsx
+++ src/components/otherui/Table.tsx
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b transition-all delay-100 duration-200 ease-in-out hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
     {...props}
