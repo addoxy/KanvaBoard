c042b3536b41bc8417daf721f833d5c50c70caf4
addoxy <adityakumar13043@gmail.com>
2023-12-29 14:34:10 +0530

    Add animation to the delete button of tasks dialog

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,7 +18,8 @@ const Button = (props: ButtonProps) => {
       className={cn(
         "bg-violet-700 text-zinc-300 text-sm shrink-0 font-medium transition-all delay-100 duration-200 ease-in-out hover:bg-purple-700",
         variant === "full" && "w-full rounded-lg h-10",
-        variant === "delete" && "w-full rounded-lg h-10 bg-zinc-700",
+        variant === "delete" &&
+          "w-full rounded-lg h-10 bg-zinc-700 hover:bg-zinc-800",
         variant === "xl" && "w-44 h-11 rounded-lg",
         variant === "lg" && "w-36 h-9 rounded-lg",
         variant === "md" && "w-22 h-8 rounded-md"
