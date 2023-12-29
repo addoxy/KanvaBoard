a6ec7a1f147b2d03b86893f6182be8261dda25b8
addoxy <adityakumar13043@gmail.com>
2023-12-29 14:26:01 +0530

    Add hover states to buttons

--- src/app/projects/components/CreateBoard.tsx
+++ src/app/projects/components/CreateBoard.tsx
@@ -27,7 +27,7 @@ const CreateBoard = (props: CreateBoardProps) => {
   return (
     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
       <Dialog.Trigger disabled={!isEnabled} asChild>
-        <button className="bg-violet-700 text-zinc-300 text-sm shrink-0 w-44 h-11 rounded-lg disabled:cursor-not-allowed font-medium">
+        <button className="bg-violet-700 text-zinc-300 text-sm shrink-0 w-44 h-11 rounded-lg disabled:cursor-not-allowed font-medium transition-all delay-100 duration-200 ease-in-out hover:bg-purple-700">
           + New Project
         </button>
       </Dialog.Trigger>
