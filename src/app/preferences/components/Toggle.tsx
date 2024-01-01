c27cd2e225ffc54adaf7b00292ef7d7e46aa2fd1
addoxy <adityakumar13043@gmail.com>
2024-01-01 17:50:14 +0530

    Make the default state of dark mode toggle checked and disable it till light mode is added

--- src/app/preferences/components/Toggle.tsx
+++ src/app/preferences/components/Toggle.tsx
@@ -4,7 +4,11 @@ import * as Switch from "@radix-ui/react-switch";
 
 const Toggle = () => {
   return (
-    <Switch.Root className="w-10 h-5 bg-zinc-700 rounded-full relative data-[state=checked]:bg-violet-700 outline-none cursor-default">
+    <Switch.Root
+      checked
+      disabled
+      className="w-10 h-5 bg-zinc-700 rounded-full relative data-[state=checked]:bg-violet-700 outline-none cursor-default"
+    >
       <Switch.Thumb className="block w-5 h-5 bg-white rounded-full transition-transform duration-100 translate-x-0 will-change-transform data-[state=checked]:translate-x-5" />
     </Switch.Root>
   );
