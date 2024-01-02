0aeaf38b2d66c7d9410f485cdf8e931e3e98b25d
addoxy <adityakumar13043@gmail.com>
2024-01-02 19:07:22 +0530

    Move toaster to the top of the page wrapper

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -6,11 +6,11 @@ const PageWrapper = (props: { children: React.ReactNode }) => {
 
   return (
     <div className="w-screen max-w-screen-2xl h-screen mx-auto flex">
+      <Toaster position="top-right" />
       <Sidebar />
       <div className="px-12 pt-8 w-full h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-zinc-800 scrollbar-track-zinc-850 hover:scrollbar-thumb-zinc-700 scrollbar-round">
         {children}
       </div>
-      <Toaster position="top-right" />
     </div>
   );
 };
