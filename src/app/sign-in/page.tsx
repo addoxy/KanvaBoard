237bbf440ede1f1b272223d9acaf515304c428ff
addoxy <adityakumar13043@gmail.com>
2024-01-09 00:43:32 +0530

    Improve the responsiveness of sign-in page on smaller screens

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -6,12 +6,12 @@ import SignInButton from "./components/SignInButton";
 
 export default function page() {
   return (
-    <div className="grid place-items-center mb-48 px-10 sm:px-0">
+    <div className="grid place-items-center mb-48 px-4 sm:px-0">
       <Spacer variant="xl" />
       <Image height={120} width={120} src={logo} alt="logo" className="mb-16" />
       <div className="lg:w-150 w-full h-120 bg-zinc-800/40 border border-zinc-700 mb-14 pt-14 pb-20 rounded-2xl grid place-items-center sm:w-130">
         <span className="mb-12 text-zinc-300 text-lg">Sign in with</span>
-        <div className="flex flex-col place-items-center gap-y-4 w-full px-16 sm:px-0">
+        <div className="flex flex-col place-items-center gap-y-4 w-full px-12 sm:px-0">
           <SignInButton name="Google" brand="google" />
           <SignInButton name="Github" brand="github" />
           <SignInButton name="Discord" brand="discord" />
