6209a1e8aeba93c3c61a65d9a77fec805a7dfe73
addoxy <adityakumar13043@gmail.com>
2024-01-08 22:15:15 +0530

    Make sign-in page responsive and remove unused animations

--- src/app/sign-in/components/SignInButton.tsx
+++ src/app/sign-in/components/SignInButton.tsx
@@ -12,7 +12,7 @@ const SignInButton = (props: SignInButtonProps) => {
   const { name, brand } = props;
   return (
     <button
-      className="w-104 bg-zinc-900/60 border border-zinc-700/50 text-zinc-300 flex items-center justify-center py-6 rounded-xl gap-x-7 hover:bg-zinc-900/40"
+      className="lg:w-104 sm:w-84 w-full bg-zinc-900/60 border border-zinc-700/50 text-zinc-300 flex items-center justify-center py-6 rounded-xl gap-x-7 hover:bg-zinc-900/40"
       onClick={() => signIn(brand)}
     >
       {brand === "google" && <GoogleIcon className="h-8 w-8 text-zinc-300" />}
