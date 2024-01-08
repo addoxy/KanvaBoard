0f8d482a6ad754ec2d68d0e6374d85d3fe3a33f4
addoxy <adityakumar13043@gmail.com>
2024-01-08 18:42:27 +0530

    Update board loading skeleton for lg screens

--- src/app/projects/board/[id]/components/LoadingSkeleton.tsx
+++ src/app/projects/board/[id]/components/LoadingSkeleton.tsx
@@ -3,17 +3,11 @@ const LoadingSkeleton = () => {
     <div className="flex flex-col">
       <div className="w-160 bg-zinc-800/30 rounded-lg animate-pulse h-12 mb-16"></div>
       <div className="flex gap-x-6">
-        <ColumnSkeleton />
-        <ColumnSkeleton />
-        <ColumnSkeleton />
+        <div className="w-80 bg-zinc-800/30 rounded-lg animate-pulse h-100" />
+        <div className="w-80 bg-zinc-800/30 rounded-lg animate-pulse h-100" />
+        <div className="w-80 bg-zinc-800/30 rounded-lg animate-pulse h-100 lg:hidden xl:block" />
       </div>
     </div>
-  );
-};
-
-const ColumnSkeleton = () => {
-  return (
-    <div className="w-80 bg-zinc-800/30 rounded-lg animate-pulse h-100"></div>
   );
 };
 
