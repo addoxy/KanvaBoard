cba162f94aef2cdf0919aae5efe18aee2974c572
addoxy <adityakumar13043@gmail.com>
2024-01-09 23:24:05 +0530

    Fix icon errors

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -276,9 +276,9 @@ export const DragFeatureIcon = (props: IconProps) => {
       <path
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M8 10a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2zM4 4v.01M8 4v.01M12 4v.01M16 4v.01M4 8v.01M4 12v.01M4 16v.01"
       />
     </svg>
