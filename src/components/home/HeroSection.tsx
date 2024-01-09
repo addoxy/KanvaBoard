3746b277126a3a0e4a9f1fe785d838fdf3369790
addoxy <adityakumar13043@gmail.com>
2024-01-09 17:37:12 +0530

    Add hero image

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,3 +1,5 @@
+import hero from "@/../public/hero.png";
+import Image from "next/image";
 import { ArrowIcon } from "../Icons";
 import Spacer from "../Spacer";
 
@@ -6,7 +8,7 @@ const HeroSection = () => {
     <div className="flex flex-col items-center">
       <h1 className="font-medium text-6xl text-zinc-200 text-center w-200">
         The{" "}
-        <span className="bg-gradient-to-r from-purple-700 to-fuchsia-700 inline-block text-transparent bg-clip-text">
+        <span className="bg-gradient-to-r from-purple-700 to-rose-700 inline-block text-transparent bg-clip-text">
           Minimal
         </span>{" "}
         Approach to Project Management
@@ -25,6 +27,13 @@ const HeroSection = () => {
           <ArrowIcon className="w-5 h-5 text-zinc-900 rotate-90 group-hover:translate-x-2 transition-all delay-100 duration-500 ease-in-out" />
         </button>
       </div>
+      <Spacer variant="xl" />
+      <Image
+        src={hero}
+        alt="hero"
+        className="rounded-lg border border-zinc-800 shadow-lg shadow-purple-900 w-11/12"
+      />
+      <Spacer variant="xl" />
     </div>
   );
 };
