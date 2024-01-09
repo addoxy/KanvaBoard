7e4ea8aecd2f3bf5c5d5d53ffa31b6f879ae79a6
addoxy <adityakumar13043@gmail.com>
2024-01-09 23:34:56 +0530

    Change the colors of the sign-in panel

--- src/app/sign-in/components/SignInButton.tsx
+++ src/app/sign-in/components/SignInButton.tsx
@@ -12,7 +12,7 @@ const SignInButton = (props: SignInButtonProps) => {
   const { name, brand } = props;
   return (
     <button
-      className="lg:w-104 sm:w-84 w-full bg-zinc-900/60 border border-zinc-700/50 text-zinc-300 flex items-center justify-center py-6 rounded-xl gap-x-7 hover:bg-zinc-900/40"
+      className="lg:w-104 sm:w-84 w-full bg-zinc-800/40 border border-zinc-700 text-zinc-300 flex items-center justify-center py-6 rounded-xl gap-x-7 hover:bg-zinc-800/90"
       onClick={() => signIn(brand)}
     >
       {brand === "google" && <GoogleIcon className="h-8 w-8 text-zinc-300" />}
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -9,7 +9,7 @@ export default function page() {
     <div className="grid place-items-center mb-48 px-4 sm:px-0">
       <Spacer variant="xl" />
       <Image height={120} width={120} src={logo} alt="logo" className="mb-16" />
-      <div className="lg:w-150 w-full h-120 bg-zinc-800/40 border border-zinc-700 mb-14 pt-14 pb-20 rounded-2xl grid place-items-center sm:w-130">
+      <div className="lg:w-150 w-full h-120 bg-zinc-900 border border-zinc-700 mb-14 pt-14 pb-20 rounded-2xl grid place-items-center sm:w-130">
         <span className="mb-12 text-zinc-300 text-lg">Sign in with</span>
         <div className="flex flex-col place-items-center gap-y-4 w-full px-12 sm:px-0">
           <SignInButton name="Google" brand="google" />
