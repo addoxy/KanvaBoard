93bc215d340fadeeb44bb5d5aa3a1cf2239757c6
addoxy <adityakumar13043@gmail.com>
2024-01-09 22:38:22 +0530

    Add subtle features section

--- src/components/home/FeatureCard.tsx
+++ src/components/home/FeatureCard.tsx
@@ -1,16 +1,25 @@
+import { cn } from "@/utils/utils";
+
 interface FeatureCardProps {
   FeatureIcon: React.ReactNode;
   title: string;
   content: string;
+  variant: "feature" | "subtle";
 }
 
 const FeatureCard = (props: FeatureCardProps) => {
-  const { FeatureIcon, title, content } = props;
+  const { FeatureIcon, variant, title, content } = props;
   return (
     <div className="hover:scale-105 transition-all duration-200">
       <div className="flex flex-col bg-zinc-900 border border-zinc-800 p-10 rounded-lg card relative group h-70">
         <div className="group-hover:absolute -z-10 rounded-lg -inset-0.5 rotate-card"></div>
-        <div className="relative w-12 h-12 bg-zinc-700/50 rounded-lg flex items-center justify-center mb-6">
+        <div
+          className={cn(
+            "relative w-12 h-12 bg-zinc-700/50 rounded-lg flex items-center justify-center mb-6",
+            variant === "feature" && "bg-zinc-700/50",
+            variant === "subtle" && "bg-purple-300"
+          )}
+        >
           {FeatureIcon}
         </div>
         <h3 className="relative text-xl text-zinc-200 mb-7">{title}</h3>
--- src/components/home/FeatureSection.tsx
+++ src/components/home/FeatureSection.tsx
@@ -1,7 +1,9 @@
 import {
   CustomizeFeatureIcon,
   DragFeatureIcon,
+  FavoriteIcon,
   SearchIcon,
+  SidebarIcon,
   TemplateFeatureIcon,
 } from "../Icons";
 import Spacer from "../Spacer";
@@ -24,16 +26,19 @@ const FeatureSection = () => {
           FeatureIcon={
             <TemplateFeatureIcon className="w-6 h-6 text-zinc-300" />
           }
+          variant="feature"
           title="Collection of templates"
           content="Simplify planning by leveraging a variety of ready-made templates tailored to different project needs"
         />
         <FeatureCard
           FeatureIcon={<SearchIcon className="w-6 h-6 text-zinc-300" />}
+          variant="feature"
           title="Robust search"
           content="Quickly find the project you are looking for using our robust search feature and save the time spent scrolling through lists"
         />
         <FeatureCard
           FeatureIcon={<DragFeatureIcon className="w-6 h-6 text-zinc-300" />}
+          variant="feature"
           title="Powerful drag and drop"
           content="Easily sort your tasks or columns using our easy-to-use and powerful drag and drop feature"
         />
@@ -41,6 +46,7 @@ const FeatureSection = () => {
           FeatureIcon={
             <CustomizeFeatureIcon className="w-6 h-6 text-zinc-300" />
           }
+          variant="feature"
           title="Make it your own"
           content="Tailor your experience with the wide variety of preferences, putting you in control of the app's look and feel"
         />
@@ -54,6 +60,24 @@ const FeatureSection = () => {
         </span>{" "}
         Impact
       </h2>
+      <Spacer variant="xl" />
+      <div className="grid grid-cols-2 gap-x-10 px-50 gap-y-10">
+        <FeatureCard
+          FeatureIcon={
+            <SidebarIcon className="w-6 h-6 text-purple-800 rotate-180" />
+          }
+          variant="subtle"
+          title="Toggle your sidebar"
+          content="You can set your preferences to have a toggleable sidebar to ensure complete focus on the task at hand"
+        />
+        <FeatureCard
+          FeatureIcon={<FavoriteIcon className="w-6 h-6 text-purple-800" />}
+          variant="subtle"
+          title="Favorites section"
+          content="Add upto five projects to favorites and never lose sight of your most important projects"
+        />
+      </div>
+      <Spacer variant="xl" />
       <Spacer variant="xl" />
     </>
   );
