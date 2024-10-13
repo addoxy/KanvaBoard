5f34963971b5f2b0391d2bba4b6f2d525505e46e
Aditya Kumar <adityakumar13043@gmail.com>
2024-10-13 20:44:05 +0000

    Add email templates

---
+++ src/components/email-templates/verification-email.tsx
@@ -0,0 +1,75 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components';
+
+type VerificationEmailProps = {
+  verificationEmailLink?: string;
+};
+
+export const VerificationEmail = ({ verificationEmailLink }: VerificationEmailProps) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>KanvaBoard verify your email</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Img
+            src={`${process.env.NEXT_PUBLIC_URL}/public/logo.png`}
+            width="40"
+            height="33"
+            alt="KanvaBoard"
+          />
+          <Section>
+            <Text style={text}>
+              Click on the following link to verify your email and continue using your account:
+            </Text>
+            <Button style={button} href={verificationEmailLink}>
+              Verify email
+            </Button>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+const main = {
+  backgroundColor: '#f6f9fc',
+  padding: '10px 0',
+};
+
+const container = {
+  backgroundColor: '#ffffff',
+  border: '1px solid #f0f0f0',
+  padding: '45px',
+};
+
+const text = {
+  fontSize: '16px',
+  fontFamily:
+    "'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif",
+  fontWeight: '300',
+  color: '#404040',
+  lineHeight: '26px',
+};
+
+const button = {
+  backgroundColor: '#007ee6',
+  borderRadius: '4px',
+  color: '#fff',
+  fontFamily: "'Open Sans', 'Helvetica Neue', Arial",
+  fontSize: '15px',
+  textDecoration: 'none',
+  textAlign: 'center' as const,
+  display: 'block',
+  width: '210px',
+  padding: '14px 7px',
+};
--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,5 @@
+import { ResetPasswordEmail } from '@/components/email-templates/reset-password';
+import { VerificationEmail } from '@/components/email-templates/verification-email';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -6,10 +8,10 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmationLink = `${process.env.NEXT_PUBLIC_URL}/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@kanvaboard.addoxy.me',
+    from: 'KanvaBoard <kanvaboard.addoxy.me>',
     to: email,
     subject: 'Verify your email',
-    html: `<p>Click <a href=${confirmationLink}>here to confirm email</a></p>`,
+    react: VerificationEmail({ verificationEmailLink: confirmationLink }),
   });
 };
 
@@ -17,9 +19,9 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetPasswordLink = `${process.env.NEXT_PUBLIC_URL}/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@kanvaboard.addoxy.me',
+    from: 'KanvaBoard <kanvaboard.addoxy.me>',
     to: email,
     subject: 'Reset your password',
-    html: `<p>Click <a href=${resetPasswordLink}>here to reset password</a></p>`,
+    react: ResetPasswordEmail({ resetPasswordLink }),
   });
 };
