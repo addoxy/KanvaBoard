0413f85f4826a6c0c28287f9231e80b640a63e72
Aditya Kumar <adityakumar13043@gmail.com>
2024-10-19 13:51:14 +0000

    Add breadcrumbs

--- src/app/(protected)/components/page-header.tsx
+++ src/app/(protected)/components/page-header.tsx
@@ -1,15 +1,47 @@
 'use client';
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@/components/vendor/breadcrumb';
 import { usePathname } from 'next/navigation';
+import React from 'react';
 import MobileSidebar from './mobile-sidebar';
 
 const PageHeader = () => {
   const pathname = usePathname();
-  const formattedPath = pathname.split('-').join(' ').replace('/', '');
+  const breadcrumbs = pathname
+    .split('/')
+    .filter(Boolean)
+    .map((segment) => ({
+      href: `/${pathname
+        .split('/')
+        .filter(Boolean)
+        .slice(0, pathname.split('/').filter(Boolean).indexOf(segment) + 1)
+        .join('/')}`,
+      label: segment
+        .split('-')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' '),
+    }));
 
   return (
-    <div className="flex justify-between">
-      <h1 className="mb-8 text-xl font-semibold capitalize">{formattedPath}</h1>
+    <div className="-mx-6 mb-6 flex justify-between border-b-2 border-border/50 pb-5">
+      <Breadcrumb className="ml-6">
+        <BreadcrumbList>
+          {breadcrumbs.map((breadcrumb, i) => (
+            <React.Fragment key={breadcrumb.href}>
+              <BreadcrumbItem>
+                <BreadcrumbLink href={breadcrumb.href}>{breadcrumb.label}</BreadcrumbLink>
+              </BreadcrumbItem>
+              {i < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
       <MobileSidebar className="lg:hidden" />
     </div>
   );
